41f2298b35b1ffa2a52108ae4212d5130c6aca39
ERICK-KRAMER <kramerfilho@hotmail.com>
2024-06-29 18:35:55 -0300

    fix: warning about isRegister

--- Login/src/components/form/fromContainer.tsx
+++ Login/src/components/form/fromContainer.tsx
@@ -3,16 +3,22 @@ import { ComponentProps, ReactNode, forwardRef } from 'react';
 interface FormContainerProps extends ComponentProps<'form'> {
   children: ReactNode;
   isRegister: boolean;
-};
+}
 
 const FormContainer = forwardRef<HTMLFormElement, FormContainerProps>((props, ref) => {
+  const { isRegister, children, ...rest } = props;
+
   return (
-    <form className={`w-full md:w-1/2 py-10 px-5 md:px-10 flex flex-col h-[600px] justify-center ${props.isRegister ? 'md:-translate-x-full transition duration-500' : 'transition duration-500'}`} {...props} ref={ref}>
+    <form
+      className={`w-full md:w-1/2 py-10 px-5 md:px-10 flex flex-col h-[600px] justify-center ${isRegister ? 'md:-translate-x-full transition duration-500' : 'transition duration-500'}`}
+      ref={ref}
+      {...rest}
+    >
       <div className="text-center mb-10">
-        <h1 className="font-bold text-3xl text-gray-900">{props.isRegister ? 'REGISTER' : "LOGIN"}</h1>
-        <p>Enter your information to {props.isRegister ? 'Register' : 'Login'} </p>
+        <h1 className="font-bold text-3xl text-gray-900">{isRegister ? 'REGISTER' : "LOGIN"}</h1>
+        <p>Enter your information to {isRegister ? 'Register' : 'Login'} </p>
       </div>
-      <div className="flex flex-col w-full">{props.children}</div>
+      <div className="flex flex-col w-full">{children}</div>
     </form>
   );
 });
